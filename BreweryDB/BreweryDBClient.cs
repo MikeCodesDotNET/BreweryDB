b901dc8cc4eeaad7c6635f71a6c91abaa3b6bfd3
Mike James <mike@xamarin.com>
2015-12-14 12:20:36 +0000

    Refactored Client
    Removed unused method and field

--- BreweryDB/BreweryDBClient.cs
+++ BreweryDB/BreweryDBClient.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using BreweryDB.Models;
-using BreweryDB.Resources;
-using Newtonsoft.Json;
-using System.Net;
+﻿using BreweryDB.Resources;
 
 namespace BreweryDB
 {
@@ -12,12 +6,10 @@ namespace BreweryDB
     {
         public static string ApplicationKey { get; private set; }
         public static readonly string BaseAddress = "https://api.brewerydb.com/v2/";
-        private readonly HttpClient client;
         
         public BreweryDbClient(string key)
         {
             ApplicationKey = key;
-            client = new HttpClient();
 
             Beers = new BeerResource(this);
             Breweries = new BreweryResource(this);
@@ -37,24 +29,5 @@ namespace BreweryDB
         public SocialSiteResource SocialSites { get; set; }
         public EventResource Events { get; set; }
         public FeatureResource Features { get; set; }
-
-        internal async Task<string> FetchJson(string url)
-        {
-            var response = await client.GetAsync(url);
-            var jsonString = response.Content.ReadAsStringAsync();
-            jsonString.Wait();
-
-            if (response.StatusCode == HttpStatusCode.Unauthorized)
-            {
-                var error = JsonConvert.DeserializeObject<Error>(jsonString.Result, new JsonSerializerSettings());
-                throw error == null
-                    ? new UnauthorizedAccessException(
-                        $"Failed to authorize with BreweryDB. This is most likely due to an invalud URL. {url}")
-                    : new UnauthorizedAccessException(
-                        $"Error to authorize with BreweryDB. Returned error message : {error.Message}\r\n URL: {url} ");
-            }
-
-            return jsonString.Result;
-        }
     }
 }
